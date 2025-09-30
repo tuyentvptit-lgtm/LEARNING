1f9073644e08dffa3fc171f6dfd2a7fe36a50886
TuyenChun <tuyentv.ptit@gmail.com>
2025-09-30 22:59:23 +0700

    test commit

--- HOC TAP.py
+++ HOC TAP.py
@@ -1,5 +1,13 @@
-x = int(input('Nhập số nguyên: '))
-if x %2 == 0:
-    print('x là số chẵn')
-else:
-    print('x là số lẻ')
+def max_num(x, y, z):
+    if x >y and x > z:
+        return x
+    elif y > x and y > z:
+        return y
+    elif z > y and z > x:
+        return z
+
+x = int(input())
+y = int(input())
+z = int(input())
+max = max_num(x, y ,z)
+print('Số lớn nhất trong 3 số là: ', max) 
