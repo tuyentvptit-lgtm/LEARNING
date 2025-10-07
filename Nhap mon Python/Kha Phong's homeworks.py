70f7cecc022544ca9cbc152ecfaebb8b21263d22
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-08 05:45:45 +0700

    Go

--- Nhap mon Python/Kha Phong's homeworks.py
+++ Nhap mon Python/Kha Phong's homeworks.py
@@ -1,12 +1,14 @@
-# Ngày tháng năm sinh của An
-a, b, c = map(int, input().split())
-#Ngày tháng năm sinh của ny An
-d, e, f = map(int, input().split())
-if c < f:
-    print("1")
-elif c == f and b < e:
-    print("1")
-elif c == f and b == e and a < d:
-    print("1")
+a, b, c, d, e =  map(int, input().split())
+numbers = [a, b, c, d, e]
+cnt = [0]* 1001
+found = True
+for i in numbers:
+    cnt[i] += 1
+    if cnt[i] > 1:
+        found = True
+    else:    
+        found = False
+if found == True:
+    print('NO')
 else:
-    print('2')
+    print('YES')
