a13968dc63b5bf839e7c34818144693217955dcf
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-07 08:37:04 +0700

    Go

--- Nhap mon Python/Kha Phong's homeworks.py
+++ Nhap mon Python/Kha Phong's homeworks.py
@@ -1,2 +1,8 @@
-print('**********')
-print('**********')
+expr = input().strip()
+left, right = expr.split('=')
+left = left.strip()
+right = right.strip()
+if eval(left) == int(right):
+    print(10)
+else:
+    print(0)
