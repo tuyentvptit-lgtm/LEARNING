8e1eb8ec12569a856df30619d030104b4fb0265f
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-07 08:39:53 +0700

    Go

--- Nhap mon Python/Kha Phong's homeworks.py
+++ Nhap mon Python/Kha Phong's homeworks.py
@@ -1,8 +1,8 @@
-expr = input().strip()
-left, right = expr.split('=')
-left = left.strip()
-right = right.strip()
-if eval(left) == int(right):
+expr = input().strip()    
+ans = input().strip()      
+result = eval(expr)
+if result == float(ans):   
     print(10)
 else:
     print(0)
+
