46a62a7c97c0e3a5d9b9b717b917ab3a2e2e437c
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-11 06:33:14 +0700

    Go

--- Nhap mon Python/Kha Phong's homeworks.py
+++ Nhap mon Python/Kha Phong's homeworks.py
@@ -1,7 +1,2 @@
-T = int(input()) #số testcase
-for i in range(1, T+1):
-    X = int(input()) #Nhập nhiệt độ
-    if X >= 30:
-        print('Yes')
-    else:
-        print('No')
+n = int(input())
+print(list(range(n)))
