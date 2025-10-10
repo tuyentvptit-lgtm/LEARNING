20ca7e6642e2c54bf8d5cc56f1a82f2601347dd3
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-11 05:59:30 +0700

    Go

--- Nhap mon Python/Kha Phong's homeworks.py
+++ Nhap mon Python/Kha Phong's homeworks.py
@@ -1,14 +1,4 @@
-a, b, c, d, e =  map(int, input().split())
-numbers = [a, b, c, d, e]
-cnt = [0]* 1001
-found = True
-for i in numbers:
-    cnt[i] += 1
-    if cnt[i] > 1:
-        found = True
-    else:    
-        found = False
-if found == True:
-    print('NO')
-else:
-    print('YES')
+n = int(input()) #Nhập số táo
+m = int(input()) #Số người cháu
+Apple = n//m
+print( Apple)
