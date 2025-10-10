f58ea406bbccf46223de7744db4c9f0ed5e9c8a7
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-11 06:28:47 +0700

    Go

--- Nhap mon Python/Kha Phong's homeworks.py
+++ Nhap mon Python/Kha Phong's homeworks.py
@@ -1,4 +1,7 @@
-n = int(input()) #Nhập số táo
-m = int(input()) #Số người cháu
-Apple = n//m
-print( Apple)
+T = int(input()) #số testcase
+for i in range(1, T+1):
+    X = int(input()) #Nhập nhiệt độ
+    if X >= 30:
+        print('Yes')
+    else:
+        print('No')
