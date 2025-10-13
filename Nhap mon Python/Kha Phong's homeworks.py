b6f0e3d50b4bd65e91d9f945cc9787ca55b6c843
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-14 00:59:09 +0700

    Go

--- Nhap mon Python/Kha Phong's homeworks.py
+++ Nhap mon Python/Kha Phong's homeworks.py
@@ -1,13 +1,9 @@
-#Nhập vào n (0<=n<=10^18), in ra số lượng chữ số chẵn, lẻ của n.
-#Yêu cầu nhập số bộ test
-t = int(input())
-for i in range(t):
-    n = int(input())
-    count = 0
-    d = 0
-    while n > 0:
-        d += n % 10
-        if n > 0:
-            count += 1
-        n //= 10
-    print(count)
+a, b, c, k =  map(float, input().split())
+T = a + b + c
+if k == 1:
+    d = 0.75
+elif k == 2:
+    d = 0.5
+point = (30 - T)/7.5*d
+diem = round(point)
+print(diem)
