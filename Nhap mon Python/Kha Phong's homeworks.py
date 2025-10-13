f4ebb58d2fa8c4a41551bd97172f7ff0008b2758
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-14 00:33:14 +0700

    Go

--- Nhap mon Python/Kha Phong's homeworks.py
+++ Nhap mon Python/Kha Phong's homeworks.py
@@ -1,4 +1,16 @@
+#Nhập vào n (0<=n<=10^18), in ra số lượng chữ số chẵn, lẻ của n.
+#Yêu cầu nhập số bộ test
 t = int(input())
-for _ in range(t):
-    n = input().strip()
-    print(sum(int(ch) for ch in n))
+for i in range(t):
+    n = int(input())
+    chan = 0
+    le = 0
+    while n > 0:
+        d = n % 10
+        if d % 2 == 0:
+            chan += 1
+        else:
+            le += 1
+        n //= 10
+    print(chan, le)
+   
