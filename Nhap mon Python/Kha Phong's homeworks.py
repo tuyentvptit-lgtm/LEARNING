c6b61d9662958eb4c74c80933c931adec168a2d0
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-13 22:45:35 +0700

    Go

--- Nhap mon Python/Kha Phong's homeworks.py
+++ Nhap mon Python/Kha Phong's homeworks.py
@@ -1,13 +1,8 @@
-a = float(input())
-b = float(input())
-c = float(input())
-dtb = (a + b*2 + c)/4
-print(dtb)
-if dtb >= 8:
-    print('gioi')
-elif dtb >= 6.5:
-    print('kha banh')
-elif dtb >= 5:
-    print('trung binh')
+a, b, c = map(int, input().split())
+if a + b > c and a +c > b and b + c >a:
+    if a!= b and b != c and c != a:
+        print('YES')
+    else:
+        print('NO')
 else:
-    print('yeu')
+    print('NO')
