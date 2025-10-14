4090f182784a4fdc99c485da10b4b8219b7c95a3
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-14 07:03:57 +0700

    Go

--- Nhap mon Python/Kha Phong's homeworks.py
+++ Nhap mon Python/Kha Phong's homeworks.py
@@ -1,8 +1,16 @@
-t = int(input())
-for i in range(t):
-    n = int(input())
-    tong = 0
-    while n > 0:
-        tong += n%10
-        n//=10
-    print(tong)
+import math
+a, b = map(int, input().split())
+c =  a*a + b*b
+def prime(c):
+    if c < 2:
+            return False
+    for i in range(2, math.isqrt(c)+1):
+        
+        if c % i == 0:
+            return False
+    return True
+
+if prime(c)== False:
+    print('NO')
+else:
+    print('YES') 
