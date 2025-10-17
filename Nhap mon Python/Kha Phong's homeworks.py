37849e00d7cac3d1e69fce76f955049613af2c71
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-17 10:17:44 +0700

    Go

--- Nhap mon Python/Kha Phong's homeworks.py
+++ Nhap mon Python/Kha Phong's homeworks.py
@@ -1,16 +1,6 @@
-import math
-a, b = map(int, input().split())
-c =  a*a + b*b
-def prime(c):
-    if c < 2:
-            return False
-    for i in range(2, math.isqrt(c)+1):
-        
-        if c % i == 0:
-            return False
-    return True
-
-if prime(c)== False:
-    print('NO')
-else:
-    print('YES') 
+str = input().strip()
+result = ''
+for char in str:
+    if not char.isdigit():
+        result += char
+print( result)
