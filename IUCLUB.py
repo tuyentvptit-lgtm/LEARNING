ce97d6b097509b402bec0216450f16d6350974b1
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-19 15:25:06 +0700

    Go

---
+++ IUCLUB.py
@@ -0,0 +1,5 @@
+n = int(input())
+m = input()
+for i in range (1, n + 1):
+    if i :
+        print(i)
--- School.py
+++ School.py
@@ -1,20 +1,8 @@
-age = 20
-country = 'Chinese'
-if age >= 18:
-    if country == 'Chinese':
-        print('bad guy')
-    else:
-        print('good guy')
-print('Đủ tuổi đi tù')
-
-if age >= 18 and country == 'Chinese':
-    print('Người xấu')
-else:
-    print('Người tốt')
-
-for i in range(1, 101):
-    print(i)
-sum = 0
-for j in range(1, 11):
-    sum += j
-print(sum)
+a, b = map(int, input().split())
+for i in range(a, b + 1):
+    if i > 1:
+        for j in range(2, int(i**0.5) + 1):
+            if i % j == 0:   
+                break
+        else:
+            print(i)
