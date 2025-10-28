1d18b307809af162425ecec462f2f2a2d7d1e432
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-28 22:25:22 +0700

    Go

--- IUCLUB.py
+++ IUCLUB.py
@@ -1,14 +1,25 @@
 #Reverse String
-#class Solution:
-#    def reverseString(self, s: List[str]) -> None:
-#        """
-#        Do not return anything, modify s in-place instead.
-#        """
-#
-#        s.reverse()
-    
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+
+        s.reverse()
 
 #Length of Last Word
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        s.length()
+        s = s.strip()
+        word = s.split(" ")
+        return len(word[-1])
+    
+#Remove consonants from a string
+def remove(s):
+    a = ''
+    for char in s:
+        if char in 'aeiouAEIOU':
+            a += char
+    return a
+
+#
