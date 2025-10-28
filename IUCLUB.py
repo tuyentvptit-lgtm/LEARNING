fa944a4fd3ee672eee5525c88a0b8899024da234
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-28 22:11:32 +0700

    Go

--- IUCLUB.py
+++ IUCLUB.py
@@ -1,3 +1,14 @@
-n = list(map(str, input().split()))
-n.reverse()
-print(n)
+#Reverse String
+#class Solution:
+#    def reverseString(self, s: List[str]) -> None:
+#        """
+#        Do not return anything, modify s in-place instead.
+#        """
+#
+#        s.reverse()
+    
+
+#Length of Last Word
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s.length()
