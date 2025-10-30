7187c0f06d54917d7b1635bf4d30ca4e8cd84e10
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-30 08:01:02 +0700

    Go

--- School.py
+++ School.py
@@ -1,8 +1,18 @@
-a, b = map(int, input().split())
-for i in range(a, b + 1):
-    if i > 1:
-        for j in range(2, int(i**0.5) + 1):
-            if i % j == 0:   
-                break
-        else:
-            print(i)
+dict = {
+    'Hi': 'Chào',
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4
+}
+print(dict['Hi'])
+print(dict.get('one'))
+print(dict.get('ten', 10))
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+school = {
+    'cl1': {'num': 45, 'male': 40},
+    'cl2': {'num': 50, 'female': 3}
+}
+print(school['cl1']['male'])
