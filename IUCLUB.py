8327803c825f3e203b41974986c59d5c99922e32
TuyenChun <tuyentv.ptit@gmail.com>
2025-10-31 13:12:43 +0700

    Go

--- IUCLUB.py
+++ IUCLUB.py
@@ -1,37 +1,6 @@
-#Reverse String
-class Solution:
-    def reverseString(self, s: list[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-
-        s.reverse()
-
-#Length of Last Word
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        s = s.strip()
-        word = s.split(" ")
-        return len(word[-1])
-    
-#Remove consonants from a string
-def remove(s):
-    a = ''
-    for char in s:
-        if char in 'aeiouAEIOU':
-            a += char
-        else: 
-            print('No Vowel')
-    return a
-
-#Replace all 0's with 5
-class Solution:
-    def convertFive(self, n):
-        n = str(n)
-        num = ''
-        for ch in n:
-            if ch == '0':
-                num += '5'
-            else:
-                num += ch
-        return int(num)
+n = input()
+ch = ''
+for i in n:
+    if i not in '123456789':
+        ch += i
+print(ch)
