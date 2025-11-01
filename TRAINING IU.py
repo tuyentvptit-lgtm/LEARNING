b92464d3638a47071b7707bc0f8c00202b1b6fcf
TuyenChun <tuyentv.ptit@gmail.com>
2025-11-01 16:56:36 +0700

    Go

--- TRAINING IU.py
+++ TRAINING IU.py
@@ -1,8 +1,10 @@
-nums = list(map(int, input().split()))
-k = 1
-for i in range(1, len(nums)):
-    if nums[i] != nums[i - 1]:
-        nums[k] = nums[i]
-        k += 1
-    print(k)
-    print(nums)
+import math
+def prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+n = int(input())
+print(prime(n))
