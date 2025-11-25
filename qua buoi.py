a9e2f9c8bbc80b3c580466b3fc2b68d9747a38cd
TuyenChun <tuyentv.ptit@gmail.com>
2025-11-25 16:34:13 +0700

    Go

--- qua buoi.py
+++ qua buoi.py
@@ -1,14 +1,12 @@
 x, y, z = map(int, input().split())
 count = 0
-if x % z != 0 and y % z != 0 and x < y:
+buoi = x // z + y // z
+if x < y and x % z != 0:
     x += 1
-    y -= 1
     count += 1
-    print((y // z) + (x // z), count)
-elif x % z != 0 and y % z != 0 and x > y:
-    x -= 1
+elif x > y and y % z != 0:
     y += 1
     count += 1
-    print((x // z) + (y // z), count)
+print(buoi, count)
     
     
