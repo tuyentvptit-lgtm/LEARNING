143f448befa5706aabbf186878416351de460775
TuyenChun <tuyentv.ptit@gmail.com>
2025-12-04 03:45:50 +0700

    Go

---
+++ Nhap mon Python/Dict.py
@@ -0,0 +1,15 @@
+tu_dien_anh_viet = {
+    'hello': 'xin chao',
+    'thank' : 'cam on',
+}
+fruit = {
+    'berry':{'red': 'strawberry', 'blue': 'blueberry'},
+    'veget':{'circle': 'bapcai', 'cai': 'dang' },
+}
+#print(tu_dien_anh_viet['hello'])
+#a = tu_dien_anh_viet.get('thank')
+#print(a)
+
+print(fruit['berry']['red'])
+fruit['meat']=['pork']
+print(fruit.items())
