bfb7f2cd0b52d70d7b8d2dfd42242b1e1963c773
TuyenChun <tuyentv.ptit@gmail.com>
2025-12-18 07:58:26 +0700

    Go

--- Nhap mon Python/Bập bẹ.py
+++ Nhap mon Python/Bập bẹ.py
@@ -1,10 +1,8 @@
-m, x = map(int, input().split())
-n = list(map(int, input().split()))
-count = 0
-left = 0
-right = len(n) - 1
-for left in range(len(n)):
-    for right in range(left + 1, len(n) + 1):
-        if sum(n[left:right]) == x:
-            count += 1
-print(count) 
+import os 
+import datetime
+today = datetime.date.today()
+folder_name = f"log_{today}"
+if not os.path.exists(folder_name):
+    os.mkdir(folder_name)
+current_dir = os.getcwd()
+print(current_dir)
