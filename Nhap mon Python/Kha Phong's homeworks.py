73a26532439e89129ef383b9c2deb5bb35bb9e6f
TuyenChun <tuyentv.ptit@gmail.com>
2026-01-07 20:51:40 +0700

    Go

--- Nhap mon Python/Kha Phong's homeworks.py
+++ Nhap mon Python/Kha Phong's homeworks.py
@@ -1,5 +1,6 @@
-n = int(input())
-for i in range(n, 0, -1):
-    for j in range(1, i + 1):
-        print(j, end='')
-    print()
+a = int(input())
+b = int(input())
+c = int(input())
+mx = max(a, b, c)
+mn = min(a, b, c)
+print(mx, mn)   
