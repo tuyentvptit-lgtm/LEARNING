19ae4ea8028b2c1efed88158ced73c06bc6d284a
TuyenChun <tuyentv.ptit@gmail.com>
2026-01-07 20:50:35 +0700

    Go

--- Nhap mon Python/Kha Phong's homeworks.py
+++ Nhap mon Python/Kha Phong's homeworks.py
@@ -1,6 +1,5 @@
-a = input()
-b = ''
-for i in a:
-    if i != 'A':
-        b += i
-print(b)
+n = int(input())
+for i in range(n, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end='')
+    print()
