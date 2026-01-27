6e3311b865f14b80e623118ef71245b98fbadcd7
TuyenChun <tuyentv.ptit@gmail.com>
2026-01-27 10:53:38 +0700

    Go

--- Nhap mon Python/JavaScript/Hoc.js
+++ Nhap mon Python/JavaScript/Hoc.js
@@ -1,3 +1,12 @@
-let a;
+let c;
 const B = 0; //khai báo biến cố định, hằng số viết in hoa
-let userName = "Tuyen" // đặt tên biến cable case
+let userName = "Tuyen" // đặt tên biến cable case   
+let a = 5,
+    b = 2;
+console.log(a + b); // Cộng
+console.log(a - b); // Trừ
+console.log(a * b); // Nhân
+console.log(a / b); // Chia
+console.log(a % b); // Chia lấy dư
+console.log(a ** b); // Lũy thừa
+print(a)
