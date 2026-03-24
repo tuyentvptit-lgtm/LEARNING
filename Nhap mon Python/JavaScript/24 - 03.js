e260af0a01229aea5ba2a3b7d188681ec83fa267
TuyenChun <tuyentv.ptit@gmail.com>
2026-03-24 09:37:00 +0700

    Go

---
+++ Nhap mon Python/JavaScript/24 - 03.js
@@ -0,0 +1,9 @@
+let mang = [1, 2, 3, 4, 5]
+mang.forEach((Element, index, array) =>{ //không trả về mảng mới, không thay đổi giá trị mảng ban đầu
+    // Thực hiện hành động với từng phần tử ở đây
+})
+
+//map(): tạo mảng mới = cách tùy chỉnh mảng khác
+let a = mang.map((e, i) => {
+     return e = e + 1
+})
